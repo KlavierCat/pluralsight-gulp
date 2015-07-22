42a170c8a93d437027cfdf355ca347ad23751217
klaviercat <klaviercat@plins-MacBook-Air.local>
2015-07-22 19:59:13 +0100

    npm install --save-dev gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
-var gulpprint = require('gulp-print');//print out all the files gulp touches
-var gulpif = require('gulp-if');//add condition
-var args = require('yargs').argv;//enable passing argument in the command line
+var args = require('yargs').argv;// enable passing argument in the command line
+
+var $ = require('gulp-load-plugins')({lazy: true});// load plugins as is needed
+
+// var jshint = require('gulp-jshint');
+// var jscs = require('gulp-jscs');
+// var util = require('gulp-util');
+// var gulpprint = require('gulp-print');// print out all the files gulp touches
+// var gulpif = require('gulp-if');// add condition
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHint and JSCS');
@@ -13,21 +16,21 @@ gulp.task('vet', function() {
             './src/**/*.js',
             './*.js'
         ])
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(gulpif(args.verbose, gulpprint()))
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe(jshint.reporter('fail'));
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
 function log(msg) {
     if (typeof(msg) === 'object') {
         for (var item in msg) {
             if (msg.hasOwnProperty(item)) {
-                util.log(util.colors.blue(msg[item]));
+                $.util.log($.util.colors.blue(msg[item]));
             }
         }
     } else {
-        util.log(util.colors.blue(msg));
+        $.util.log($.util.colors.blue(msg));
     }
 }
