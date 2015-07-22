0f5bf973052f41e9bdd67c42e39a79a3f1c861bf
klaviercat <klaviercat@plins-MacBook-Air.local>
2015-07-22 19:51:19 +0100

    npm install --save-dev gulp-jshint gulp-jscs gulp-util gulp-print  gulp-if yargs

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,33 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var jscs = require('gulpjscs');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');//print out all the files gulp touches
+var gulpif = require('gulp-if');//add condition
+var args = require('yargs').argv;//enable passing argument in the command line
 
-gulp.task('hello-world', function(){
-	console.log("Hello world!");
+gulp.task('vet', function() {
+    log('Analyzing source with JSHint and JSCS');
+    return gulp
+        .src([// pipe in all the source we'll be using
+            './src/**/*.js',
+            './*.js'
+        ])
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(gulpif(args.verbose, gulpprint()))
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe(jshint.reporter('fail'));
 });
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
