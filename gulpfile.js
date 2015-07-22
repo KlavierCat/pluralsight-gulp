4a0d557461c143b9ce475a0389223607eb706df5
klaviercat <klaviercat@plins-MacBook-Air.local>
2015-07-22 19:08:06 +0100

    use require to reference the gulp plugins. Pipe the stream through jscs and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var gulp = require ('gulp');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulpjscs');
 
 gulp.task('hello-world', function(){
 	console.log("Hello world!");
